6e020c97bdd02ba7046eed2aa492bf0cbce41478
Andres Correa <andres.correa@websummit.com>
2022-05-16 16:47:22 +0100

    Validation for articles and minor rubocop complaints

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# A model for an Article instance
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 2000 }
 end
