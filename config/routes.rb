ab59fee7582f97194a21b79e579a4f72ff5bf479
Andres Correa <andres.correa@websummit.com>
2022-05-17 16:56:45 +0100

    Trying out comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   root 'articles#index'
   get 'about', to: 'static_pages#about'
+  resources :articles do
+    resources :comments
+  end
+
   namespace :api do
     namespace :v1 do
       resources :articles
