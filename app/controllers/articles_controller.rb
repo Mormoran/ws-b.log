36663bcbf07b9dbd0bdad6515561c43931794783
Andres Correa <andres.correa@websummit.com>
2022-05-18 11:44:53 +0100

    Changed order to descending

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[edit update show destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order(created_at: :desc)
   end
 
   def new
